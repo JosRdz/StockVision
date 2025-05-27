85b8753fcff077162f384560687bedcad8b99833
JosRdz <rfcojosue@outlook.es>
2025-05-27 19:55:10 +0000

    update

--- ia.py
+++ ia.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Ruta fija del modelo (corrigiendo barras para Windows)
-modelo = load_model("Entrenamiento/modelo_robo.keras")
+modelo = load_model("Entrenamiento/modelo.keras")
 print("Modelo cargado correctamente")
 
 # IP fija de la cámara IP
